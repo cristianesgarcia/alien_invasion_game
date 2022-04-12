e337bf745a7b261ffa23eda7cb2e95bb0a6526f1
cristiane <cristianesgcris@gmail.com>
2022-04-12 16:42:38 -0300

    Added the feature to level up the game.

--- settings.py
+++ settings.py
@@ -6,14 +6,13 @@ class Settings:
     """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the game's settings."""
+        """Initialize the game's static settings."""
         # Screen settings.
         self.screen_width = 640
         self.screen_heigh = 360
         self.bg_color = (230, 230, 230)
         
         # Ship settings.
-        self.ship_speed = 10
         self.ship_limit = 3
 
         # Redraw the screen when this events occur.
@@ -23,15 +22,30 @@ class Settings:
             pygame.KEYUP, }
 
         # Bullet settings.
-        self.bullet_speed = 5.0
         self.bullet_width = 3
         self.bullet_heigh = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 10
 
         # Alien settings.
+        self.fleet_drop_speed = 15
+        
+        # How quickly the game speeds up
+        self.speedup_scale = 1.5
+
+        self.initialize_dynamic_settings()
+    
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 10
+        self.bullet_speed = 5.0
         self.alien_speed = 2.0
-        self.fleet_drop_speed = 40
-                
+
         # Fleet direction of 1 represents right, -1 represents left.
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed += self.speedup_scale
+        self.bullet_speed += self.speedup_scale
+        self.alien_speed += self.speedup_scale
